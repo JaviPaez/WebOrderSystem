dfde36b0727bd6ec55dc43577ad9720544978a88
Javier Paez <javierignacio.paez@gmail.com>
2024-02-22 16:57:05 -0300

    model classes added

---
+++ WebOrderSystem/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace WebOrderSystem.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public long MemberId { get; set; }
+        public long ProductId { get; set; }
+        public DateTime DateOrder { get; set; }
+
+        public virtual Member Member { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}

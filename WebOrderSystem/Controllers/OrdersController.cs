2536c98cdfcd94dea67df77bfb01528480b0441f
Javier Paez <javierignacio.paez@gmail.com>
2024-02-22 20:21:49 -0300

    orders controller added

---
+++ WebOrderSystem/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebOrderSystem.Context;
+
+namespace WebOrderSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("LastOrdersByMember")]
+        public IActionResult GetLastOrdersByMember()
+        {
+            var lastOrdersQuery = _context.Orders
+                .Include(o => o.Member)
+                .Include(o => o.Product)
+                .GroupBy(o => o.MemberId)
+                .Select(g => g.OrderByDescending(o => o.DateOrder).FirstOrDefault());
+
+            var lastOrders = lastOrdersQuery.ToList();
+
+            var result = lastOrders.Select(o => new
+            {
+                MemberName = o.Member.Name,
+                ProductName = o.Product.Name,
+                OrderDate = o.DateOrder
+            }).OrderBy(o => o.OrderDate).ToList();
+
+            return Ok(result);
+        }
+    }
+}

3027df117fc7d530ad45619c3f2898caab348214
Javier Paez <javierignacio.paez@gmail.com>
2024-02-22 18:02:11 -0300

    migration sql script

---
+++ Database/addModelsTables.sql
@@ -0,0 +1,87 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240222195005_addModelsTables'
+)
+BEGIN
+    CREATE TABLE [Members] (
+        [Id] bigint NOT NULL IDENTITY,
+        [Name] nvarchar(500) NOT NULL,
+        CONSTRAINT [PK_Members] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240222195005_addModelsTables'
+)
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] bigint NOT NULL IDENTITY,
+        [Name] nvarchar(500) NOT NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240222195005_addModelsTables'
+)
+BEGIN
+    CREATE TABLE [Orders] (
+        [Id] bigint NOT NULL IDENTITY,
+        [MemberId] bigint NOT NULL,
+        [ProductId] bigint NOT NULL,
+        [DateOrder] datetime2 NOT NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Orders_Members_MemberId] FOREIGN KEY ([MemberId]) REFERENCES [Members] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Orders_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240222195005_addModelsTables'
+)
+BEGIN
+    CREATE INDEX [IX_Orders_MemberId] ON [Orders] ([MemberId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240222195005_addModelsTables'
+)
+BEGIN
+    CREATE INDEX [IX_Orders_ProductId] ON [Orders] ([ProductId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20240222195005_addModelsTables'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240222195005_addModelsTables', N'8.0.2');
+END;
+GO
+
+COMMIT;
+GO
+

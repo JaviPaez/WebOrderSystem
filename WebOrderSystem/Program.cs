a2c46d48240b0c0e93bbf31ff8ca8a6e57a90564
Javier Paez <javierignacio.paez@gmail.com>
2024-02-22 16:58:55 -0300

    ApplicationDbContext added

--- WebOrderSystem/Program.cs
+++ WebOrderSystem/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebOrderSystem.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
